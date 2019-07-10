73b23452df50427560b7ea7b2b0c383aec099da2
randyprempeh <rprempehs@gmail.com>
2019-07-10 18:41:01 +0200

    Updates

---
+++ precache-manifest.72e09782e0ae8281764a3697877fbb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a675341dc19479ef5e",
+    "url": "/fullstack-exercise/static/js/runtime~main.44a67534.js"
+  },
+  {
+    "revision": "e9ae888b9a7f3c74512c",
+    "url": "/fullstack-exercise/static/js/main.e9ae888b.chunk.js"
+  },
+  {
+    "revision": "0d63336e403d26e2cd0a",
+    "url": "/fullstack-exercise/static/js/1.0d63336e.chunk.js"
+  },
+  {
+    "revision": "e9ae888b9a7f3c74512c",
+    "url": "/fullstack-exercise/static/css/main.9f9c2dfa.chunk.css"
+  },
+  {
+    "revision": "0d63336e403d26e2cd0a",
+    "url": "/fullstack-exercise/static/css/1.9b04b7be.chunk.css"
+  },
+  {
+    "revision": "8c883b04e2b323bb3e3910247e1c4c2a",
+    "url": "/fullstack-exercise/index.html"
+  }
+];
