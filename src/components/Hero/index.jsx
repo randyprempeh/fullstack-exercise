0a5d25a88e2664408a4557440ba1426f4dd20359
randyprempeh <36501925+randyprempeh@users.noreply.github.com>
2019-07-11 22:09:36 +0200

    update logo

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="hero-content">
-                <div className="logo"><img src="./logo.png" alt="" /></div>
+                <div className="logo"><img src=".logo.png" alt="" /></div>
                 <h1>We bring the world closer to you</h1>
                 <Button>Get Closer</Button>
             </div>
